c9d0318786b3c4f9faa54cff34e48fd827782cb9
nbvanting <nicolai@vanting.com>
2024-10-03 14:55:06 +0200

    added groups

--- main.py
+++ main.py
@@ -5,21 +5,32 @@ from player import Player
 
 def main() -> None:
 	pygame.init()
-	clock = pygame.time.Clock()
-	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+
+	Player.containers = (updatable, drawable)
+
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+	dt = 0
+
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 
+		for obj in updatable:
+			obj.update(dt)
+
 		screen.fill(color="black")
 
-		player.draw(screen)
-		player.update(dt)
-
+		for obj in drawable:
+			obj.draw(screen)
+		
 		pygame.display.flip()
 
 		dt = clock.tick(60) / 1000
--- player.py
+++ player.py
@@ -4,7 +4,9 @@ from circleshape import CircleShape
 from constants import *
 
 class Player(CircleShape):
+
     def __init__(self, x, y):
+        
         super().__init__(x, y, radius=PLAYER_RADIUS)
         self.rotation = 0
 
