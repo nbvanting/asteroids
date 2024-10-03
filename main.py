2d652186eefe3bbb99337338c2e1aa53acc0ff53
nbvanting <nicolai@vanting.com>
2024-10-03 11:11:50 +0200

    added FPS restriction

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from constants import *
 
 def main() -> None:
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
 
 	while True:
 		for event in pygame.event.get():
@@ -14,6 +18,7 @@ def main() -> None:
 		screen.fill(color="black")
 		pygame.display.flip()
 
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
