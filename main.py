6566fa63156d45e067da96882f94f58ffcf143d7
nbvanting <nicolai@vanting.com>
2024-10-04 10:27:00 +0200

    added shooting

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
 import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main() -> None:
@@ -13,10 +15,13 @@ def main() -> None:
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
 	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
 
 	Player.containers = (updatable, drawable)
 	Asteroid.containers = (asteroids, updatable, drawable)
-	AsteroidField.containers = (updatable)
+	AsteroidField.containers = updatable
+	Shot.containers = (shots, updatable, drawable)
+
 
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	asteroidfield = AsteroidField()
@@ -31,6 +36,11 @@ def main() -> None:
 
 		for obj in updatable:
 			obj.update(dt)
+
+		for asteroid in asteroids:
+			if asteroid.is_colliding(player):
+				print("Game over!")
+				sys.exit()
 
 		screen.fill(color="black")
 
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame
 from circleshape import CircleShape
+from shot import Shot
 
 from constants import *
 
@@ -31,6 +32,10 @@ class Player(CircleShape):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -42,6 +47,7 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(dt*(-1))
-
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     
